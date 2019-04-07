b2a4a686f24f0a643745d6bc41a924a6a925a419
Michael Karpen <michael@mkarpen.com>
2019-04-06 21:46:30 -0700

    Material UI attempt
    Was working great until the textbox would refresh the page when pushing enter instead of preventing default. So commented out for now

--- src/MovieApp/components/Search.js
+++ src/MovieApp/components/Search.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+//import TextField from '@material-ui/core/TextField';
+//import Button from '@material-ui/core/Button';
 
 const Search = (props) => {
   const [searchValue, setSearchValue] = useState("");
@@ -17,12 +19,26 @@ const Search = (props) => {
 
   return (
     <form className="search">
+      {/* 
+      <TextField
+        id="outlined-name"
+        label="Search"
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+        margin="none" 
+        variant="outlined" type="text"
+      />
+      <Button variant="outlined" color="primary" onClick={callSearchFunction}>
+        Search
+      </Button>
+      */}
       <input
         value={searchValue}
         onChange={handleSearchInputChanges}
         type="text"
       />
       <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      
     </form>
   );
 }
