8d81de9c6111fde2d7b849ac74091b7ade51c921
Michael Karpen <michael@mkarpen.com>
2019-05-05 08:39:22 -0700

    Using history.push on button click
    For an example of how to use history.push in a function

--- src/RoutingApp/views/Home.js
+++ src/RoutingApp/views/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+//import { withRouter } from 'react-router-dom';
 
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-)
+const Home = (props) => { // note: props is needed for history push
+
+  useEffect(() => {
+    // redirect on component load - will never be able to access home
+    // props.history.push('/about')
+  }, []);
+
+  const sampleRedirect = () => {
+    props.history.push('/about')
+  }
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={()=>{
+          sampleRedirect()}}> go home redirect via history push
+      </button>
+    </div>
+  );
+}
 
 export default Home
