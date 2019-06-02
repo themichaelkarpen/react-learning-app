fd199d4cc0dbc290d0247e609c30bb83a0095ace
Michael Karpen <michael@mkarpen.com>
2019-06-02 14:31:51 -0700

    Light / Dark Mode enabled

--- src/RoutingApp/App.js
+++ src/RoutingApp/App.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useContext, useReducer } from "react";
 import './css/App.scss';
 //import './css/font-awesome/css/font-awesome.min.css';
+
+import useDarkMode from './hooks//use-dark-mode';
+import Toggle from './components/Toggle';
+
 import Header from './components/Header';
 import Main from './components/Main';
+
 import AppContext from './AppContext';
 import appReducer from './reducers/reducer';
 
 const App = () => {
   const initialState = useContext(AppContext);
   const [state, dispatch] = useReducer(appReducer, initialState);
+  const [darkMode, setDarkMode] = useDarkMode();
 
   useEffect(() => {
     // nothing yet
@@ -17,6 +23,7 @@ const App = () => {
   return (
     <div className="App">
       <AppContext.Provider value={{state, dispatch}}>
+        <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
         <Header />
         <Main/>
       </AppContext.Provider>
