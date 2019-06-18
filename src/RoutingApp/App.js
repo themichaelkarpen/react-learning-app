592de81487844a72583861a2a3dea78dee12e090
Michael Karpen <michael@mkarpen.com>
2019-06-18 08:29:40 -0700

    CSS Structure updates

--- src/RoutingApp/App.js
+++ src/RoutingApp/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useContext, useReducer } from "react";
 import './css/App.scss';
-//import './css/font-awesome/css/font-awesome.min.css';
 
 import useDarkMode from './hooks//use-dark-mode';
 import Toggle from './components/Toggle';
@@ -23,9 +22,17 @@ const App = () => {
   return (
     <div className="App">
       <AppContext.Provider value={{state, dispatch}}>
-        <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Header />
-        <Main/>
+        <div id="header">
+          Header
+        </div>
+        <div id="content">
+          <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Header />
+          <Main/>
+        </div>
+        <div id="footer">
+          footer
+        </div>
       </AppContext.Provider>
     </div>
   );
