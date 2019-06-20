5b88d6388bc49d0ce3798b74e992a2cca67cdf6a
Michael Karpen <michael@mkarpen.com>
2019-06-20 08:01:33 -0700

    Updates testing initialization and loading

--- src/RoutingApp/views/Home.js
+++ src/RoutingApp/views/Home.js
@@ -7,15 +7,34 @@ const Home = (props) => { // note: props is needed for history push
   const {state, dispatch} = useContext(AppContext)
 
   useEffect(() => {
+
+    // calls on load, any state item in brackets will cause to run on changes (componentDidUpdate)
+    updateGlobalElements(updateGlobalElementsComplete) // all 'master page' variables for App
+    
+  }, []);
+
+  const updateGlobalElements = (callback=null) => {
+    // update all 'master page' variables for App
     dispatch({
       type: "SET_SELECTED_PAGE",
       payload: 'home'
     })
-  }, []);
+
+    if (callback) {
+      callback()
+    }
+  }
+
+  const updateGlobalElementsComplete = () => {
+    setTimeout(function() {
+      console.log('global updates completed', state)
+    }, 2000)
+  }
 
   const sampleRedirect = () => {
     props.history.push('/about')
   }
+
   return (
     <div>
       <h1><i className="fa fa-home"></i> Home Page</h1>
